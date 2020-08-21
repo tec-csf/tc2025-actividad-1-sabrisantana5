ee5797f09c1262e682d09bea133b2633ef15aafa
Sab <Sab@MacBook-Air-de-Mac.local>
2020-08-20 23:36:02 -0500

    Problemas de segmentation fault

--- main.c
+++ main.c
@@ -40,12 +40,14 @@ int main(int argc, const char * argv[]) {
     //Inicializar menu y pacientes
     int opc = -1;
     menu * opcs = (menu *) malloc(sizeof(menu)*N);
-    cama * hospital = (cama *) malloc(sizeof(cama)*10);
-    int total_camas = 1;
+    cama * hospital = (cama *) malloc(sizeof(cama)*2);
+    int total_camas = 2;
     hospital -> id = 1;
     hospital -> disponible = 1;
+    (hospital + 1 )-> id = 2;
+    (hospital + 1 )-> disponible = 1;
 
-    //Opciiones de menu
+    //Opciones de menu
     *opcs = incorporarPaciente;
     *(opcs+1) = camaPaciente;
     *(opcs+2) = altaPaciente;
@@ -67,7 +69,7 @@ int main(int argc, const char * argv[]) {
     cama * aux1 = hospital;
     cama * final = hospital + total_camas;
 
-    for (aux1 = hospital; aux1 < final; ++aux1) {
+    for (; aux1 < final; ++aux1) {
         free(aux1-> p ->nombre);
         free(aux1-> p -> apellidos);
         free(aux1-> p -> telefono);
@@ -81,7 +83,7 @@ int main(int argc, const char * argv[]) {
 void incorporarPaciente(cama * hospital, int total_camas)
 {
     printf("Inicio total camas: %d",total_camas);
-    paciente * nueva = (paciente *) malloc(sizeof(paciente)*1);
+    paciente * nueva = (paciente *) malloc(sizeof(paciente));
     cama * aux = hospital;
     cama * final = hospital + total_camas;
 
@@ -100,21 +102,22 @@ void incorporarPaciente(cama * hospital, int total_camas)
     printf("Telefono: ");
     scanf("%s",nueva->telefono);
 
-
+    //Va pasando por todas las direcciones hasta que encuentra una disponible, cuando la encuentra se sale
     while(aux < final && aux->disponible != 1){
         aux++;
     }
     if(aux < final){
         aux -> p = nueva;
         aux -> disponible = 0;
-        printf("Nuevo paciente asignado: %s %s Edad: %d Telefono: %s Cama asignada: %d\n",nueva -> nombre, nueva -> apellidos,  nueva -> edad, nueva -> telefono, aux->id);
-    } else{
+        printf("Nuevo paciente asignado: %s %s Edad: %d Telefono: %s Cama asignada: %d\n",(aux->p) -> nombre, (aux->p) -> apellidos,  (aux->p)-> edad, (aux->p) -> telefono, aux->id);
+    }
+    /*
+    else{
       printf("Por ahora se va a sin el realloc");
         aux = hospital + total_camas + 1;
         aux -> p = nueva;
         aux -> disponible = 0;
 
-      /*
         hospital = (cama *) realloc(hospital, sizeof(cama) * (total_camas + 1));
         printf("Cuando no hay usuario total camas %d",total_camas);
         aux = hospital + total_camas + 1;
@@ -130,7 +133,6 @@ void incorporarPaciente(cama * hospital, int total_camas)
          aux -> id = total_camas;
          aux -> disponible = 1;
          */
-    }
     free(nueva->nombre);
     free(nueva->apellidos);
     free(nueva->telefono);
@@ -139,111 +141,78 @@ void incorporarPaciente(cama * hospital, int total_camas)
 
 void camaPaciente(cama *hospital,int total_camas)
 {
-    /*
-     int check_cama;
+  int check_cama;
 
-     printf("Ingresa el ID de la cama de la que deseas conocer su disponibilidad y/0 paciente asignado\n");
-     scanf("%d",&check_cama);
+  printf("Ingresa el ID de la cama de la que deseas conocer su disponibilidad y/0 paciente asignado\n");
+  scanf("%d",&check_cama);
 
-     cama * ic = camas;
-     cama * fc = camas + h.numeroCamas;
-     int encontrado = 0;
-     for(;ic<=fc;ic++){
-     if((ic -> identificador) == check_cama){
-     printf("La cama con identificador: %d \n",ic -> identificador);
-     if((ic -> estatus) == 1){
-     printf("Se encuentra disponible \n");
-     encontrado = 1;
-     }
-     if((ic -> estatus) == 0){
-     persona * inicio = pacientes;
-     persona * final = pacientes + h.noPacientes;
-     for (;inicio<=final;inicio++) {
-     if((inicio -> ident_cama)->identificador == check_cama){
-     printf("se encuentra ocupada por el paciente: %s %s . Edad: %s Telefono: %s \n",inicio -> nombre,inicio -> apellidos,inicio->edad,inicio->telefono);
-     encontrado = 1;
-     }
-     }
-     }
-     break;
-     }
-     }
-     if(encontrado == 0){
-     printf("El identificador de cama no se encuentra registrado\n");
-     }
-     */
+  cama * aux = hospital;
+  cama * final = hospital + total_camas;
+
+  while(aux < final && aux->id != check_cama){
+      aux++;
+  }
+  if(aux < final){
+      if(aux -> disponible == 1){
+        printf("La cama con ID: %d se encuentra disponible",aux->id);
+      } else{
+        printf("La cama con ID: %d se encuentra ocupada por el paciente: %s",aux->id,(aux->p)->nombre); //SEGMENTATION FAULT
+      }
+    } else{
+    printf("El identificador de cama no existe");
+  }
 }
+
 void altaPaciente(cama *hospital,int total_camas)
 {
-    /*
-     printf("Dar de alta paciente por número de cama\n");
-     int check_cama;
+  printf("Dar de alta paciente por número de cama\n");
+  int check_cama;
 
-     printf("Ingresa el ID de la cama\n");
-     scanf("%d",&check_cama);
+  printf("Ingresa el ID de la cama\n");
+  scanf("%d",&check_cama);
 
-     cama * ic = camas;
-     cama * fc = camas + h.numeroCamas;
-     int encontrado = 0;
-     for(;ic<=fc;ic++){
-     if((ic -> identificador) == check_cama){
-     encontrado = 1;
-     ic -> estatus = 1;
-     persona * inicio = pacientes;
-     persona * final = pacientes + h.noPacientes;
-     for (;inicio<=final;inicio++) {
-     if((inicio -> ident_cama)->identificador == check_cama){
-     //Eliminar persona
-     free(inicio); //Por el momento no sirve
-     printf("El paciente: %s %s se ha dado de alta exitosamente \n",inicio -> nombre,inicio -> apellidos);
-     }
-     }
-     }
-     }
-     if(encontrado==0){
-     printf("El identificador de cama no se encuentra registrado\n");
-     }
-     */
+  cama * aux = hospital;
+  cama * final = hospital + total_camas;
+
+  while(aux < final && aux->id != check_cama){
+      aux++;
+  }
+  if(aux < final){
+      aux -> disponible = 1;//Liberar cama
+      //Eliminar paciente, estos frees dan SEGMENTATION FAULT
+      free((aux->p)->nombre);
+      free((aux->p)->apellidos);
+      free((aux->p)->telefono);
+      free(aux->p);
+    } else{
+    printf("El identificador de cama no existe");
+  }
 }
 
 void listaPacientes(cama *hospital, int total_camas)
 {
-    /*
-     printf("Lista de pacientes");
+     printf("Lista de pacientes\n");
      cama * aux = hospital;
      cama * final = hospital + total_camas;
-     while(aux < final && aux->p){
-     printf("Nombre: %s\tApellidos: %s\tEdad: %s\tTeléfono: %s\t Cama asignada: %d\n", (aux->p) -> nombre, (aux->p ) -> apellidos, (aux->p)-> edad,(aux->p) -> telefono, aux->id);
-     aux++;
+     while(aux < final){
+       printf("%10d %10d\n", aux->id, aux->disponible);
+       //printf("%10d %10d %10s\n", aux->id, aux->disponible, (aux->p)->nombre); Se rompe por alguna extraña razón del universo
+      aux++;
      }
-     */
-    /*
-     printf("Lista de pacientes con cama asignada\n");
-     if( h.noPacientes == 0){
-     printf("No se encuentran pacientes registrados\n");
-     } else {
-     persona * inicio = pacientes;
-     persona * final = pacientes + h.noPacientes;
-     for (;inicio<=final;inicio++) {
-     printf("Nombre: %s\tApellidos: %s\tEdad: %s\tTeléfono: %s\t Cama asignada: %d\n", inicio -> nombre, inicio -> apellidos, inicio -> edad,inicio -> telefono, (final -> ident_cama)->identificador);
-     }
-     }
-     */
 }
 
 void camasDisp(cama *hospital,int total_camas)
 {
-    /*
-     printf("Número de camas disponibles y ocupadas\n");
-     cama * ic = camas;
-     cama * fc = camas + h.numeroCamas;
-     int contador = 0;
-     for(;ic<=fc;ic++){
-     if((ic -> estatus) == 1){
-     contador++;
-     }
-     }
-     printf("Número de camas disponibles: %d\n",contador);
-     printf("Número de camas ocupadas: %d\n",(h.numeroCamas-contador));
-     */
+  cama * aux = hospital;
+  cama * final = hospital + total_camas;
+  int contador = 0;
+  while(aux < final){
+    if(aux->disponible == 1){
+      contador++;
+    }
+  aux++;
+  }
+  printf("Número de camas disponibles: %d\n",contador);
+  printf("Número de camas ocupadas: %d\n",total_camas-contador);
+
 }
